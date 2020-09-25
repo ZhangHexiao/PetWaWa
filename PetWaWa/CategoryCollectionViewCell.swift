7d5292f71ae37516b131837b6613c256fbfd9019
hexiaozhang <hexiao.zhang@mail.mcgill.ca>
2020-09-24 22:15:01 -0400

    tabbar

---
+++ PetWaWa/CategoryCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CategoryCollectionViewCell.swift
+//  PetWaWa
+//
+//  Created by Hexiao Zhang on 2020-09-24.
+//  Copyright © 2020 Hexiao Zhang. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCollectionViewCell: UICollectionViewCell {
+    
+}
